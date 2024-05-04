ae22ed87bd339d45e39b90564ed80872083b554c
Relson Rivera <riverarelson99@gmail.com>
2024-05-03 19:46:11 -0600

    Se sube cambio creator

---
+++ pages/creator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+
+export default function Creator(){
+
+    return(
+        <Grid>
+            <Typography>
+                Sitio web desarrollado por el Ing. Relson Antonio Gutierrez Rivera
+            </Typography>
+        </Grid>
+    )
+} 
